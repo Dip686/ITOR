3e025990c87e06f5a2db87cc2bdd1308de00074f
dip686 <dip686@gmail.com>
2019-09-03 09:40:15 +0530

    removing errors

--- itor/src/reducers/editorReducer.js
+++ itor/src/reducers/editorReducer.js
@@ -5,7 +5,7 @@ let code = {
 export default function editorReducer (state = code, action) {
   switch (action.type) {
     case UPDATE_EDITOR_SRC:
-      return {...state, currentCode: action.payload};
+      return { ...state, currentCode: action.payload };
     default:
       return state;
   }
